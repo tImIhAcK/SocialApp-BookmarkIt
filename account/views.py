5e22a67426189de005ae3245df9aea92a84344df
Adeniran John <adeniranjohn2016@gmail.com>
2023-07-17 20:01:31 +0100

    Authentication

--- account/views.py
+++ account/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from .forms import LoginForm, UserRegistrationForm
+
 
 # Create your views here.
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Login successful')
+                else:
+                    return HttpResponse('Your account has been disabled.... Contact support team')
+            else:
+                return HttpResponse('Invalid username or password')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(request.POST.get('password'))
+            
+            user.save()
+            return render(request, 'account/register_done.html', {'user': user})
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    
